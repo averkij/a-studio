a5e57ea42940c2e660d14479e9b12632bdd42c75
Sergei Averkiev <averoO@gmail.com>
2021-07-16 11:12:42 +0300

    Add swiper scrollbar.

--- fe/src/plugins/vuetify.js
+++ fe/src/plugins/vuetify.js
@@ -1,6 +1,12 @@
 import "@mdi/font/css/materialdesignicons.css";
 import 'swiper/swiper-bundle.css'
 
+// import SwiperClass, { Mousewheel, Scrollbar, Lazy } from "swiper";
+import SwiperClass, { Scrollbar } from "swiper";
+// configure Swiper to use modules
+// SwiperClass.use([Mousewheel, Scrollbar, Lazy]);
+SwiperClass.use([Scrollbar]);
+
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
 import vueNumeralFilterInstaller from "vue-numeral-filter";
