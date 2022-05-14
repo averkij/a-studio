7c664e8d44378b8bb2ae5191db18ce1139bb36a3
Sergei Averkiev <averoO@gmail.com>
2022-05-14 10:23:53 +0300

    Update shell script.

--- tools/pdf/create_pdf.sh
+++ tools/pdf/create_pdf.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 html="$1.html"
+img=$(echo "$4" | sed 's/\//\\\//g')
+
+echo $img
+
+echo "Replacing image in xslt"
+
+# COVER_IMG=$4 envsubst < $2 > _temp.xslt
+
+sed -e "s/\$COVER_IMG/$img/" $2 > _temp.xslt
 
 echo "Generating html file $html"
 
-saxonb-xslt -s:$1 -xsl:$2 -o:$html
+saxonb-xslt -s:$1 -xsl:_temp.xslt -o:$html
 
-echo "Creating PDF $4"
+echo "Creating PDF $5"
 
-weasyprint -s $3 $html $4
+weasyprint -s $3 $html $5
+
+echo "Removing temp files"
 
 rm $html
+rm _temp.xslt
+
+echo "Done."
