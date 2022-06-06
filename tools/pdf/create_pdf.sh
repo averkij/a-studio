ac434d2a947690a93c03c2df06ea2169167a3020
Sergei Averkiev <averoO@gmail.com>
2022-06-06 21:32:18 +0300

    Update shell script.

--- tools/pdf/create_pdf.sh
+++ tools/pdf/create_pdf.sh
@@ -1,22 +1,94 @@
 #!/bin/bash
-html="$1.html"
-img=$(echo "$4" | sed 's/\//\\\//g')
 
-echo "Replacing image in xslt"
+#usage
+show_help() {
+cat << EOF
+Parameters:
+-----------------------------------------------------------------------------------------------------
+    -h                          Display this help and exit.
+    -i  [INPUT]                 XML file exported from Lingtrain Studio.
+    -x  [./book2html.xslt]      XSLT transformation file, which will transform XML input data to HTML
+    -s  [./book.css]            CSS style file.
+    -p  [./covers/default.jpg]  Image (3/4 ratio), which will be used as book cover.
+                                (!) Image Path should be relative to XML input data path.
+    -c  [false]                 Add color highlighting.
+    -t  [false]                 Add furigana-style tips for Chinese and Japanese texts.
+    -o  [OUTPUT]                Output PDF file.
+-----------------------------------------------------------------------------------------------------
+EOF
+}
 
-sed -e "s/\$COVER_IMG/$img/" $2 > _temp.xslt
+#default parameters
+xslt="./book2html.xslt"
+cover="./covers/default.jpg"
+css="./book.css"
+data="./book.xml"
+output_path="./result.pdf"
+show_colors="false"
+cjk_tips="false"
 
-echo "Generating html file $html"
+while getopts hi:s:x:p:o:ct opt; do
+    case $opt in
+        h)  show_help
+            exit 0
+            ;;
+        i)  data=$OPTARG
+            ;;
+        s)  css=$OPTARG
+            ;;
+        x)  xslt=$OPTARG
+            ;;
+        p)  cover=$OPTARG
+            ;;
+        o)  output_path=$OPTARG
+            ;;
+        t)  cjk_tips="true"
+            ;;
+        c)  show_colors="true"
+            ;;
+        *)  show_help >&2
+            exit 1
+            ;;
+    esac
+done
+shift "$((OPTIND-1))"
 
-saxonb-xslt -s:$1 -xsl:_temp.xslt -o:$html
+### check parameters
+cat << EOF
+-----------------------------------------------------------------------------------------------------
+Book parameters:
 
-echo "Creating PDF $5"
+    - Input file:           $data
+    - Output file:          $output_path
+    
+    - XSLT tranformation:   $xslt
+    - CSS styles:           $css
+    - Cover image:          $cover (path is relative to input file).
+    - Color highlighting:   $show_colors
+    - Tips (zh, jp langs):  $cjk_tips
+-----------------------------------------------------------------------------------------------------
+EOF
 
-weasyprint -s $3 $html $5
+### generate book
 
-echo "Removing temp files"
+html="$data.html"
+img=$(echo "$cover" | sed 's/\//\\\//g')
+
+echo "Replacing parameters in xslt..."
+
+sed -e "s/\$COVER_IMG/$img/; s/\$SHOW_COLORS/$show_colors/; s/\$CJK_TIPS/$cjk_tips/" $xslt > _temp.xslt
+
+echo "Generating html file $html..."
+
+saxonb-xslt -s:$data -xsl:_temp.xslt -o:$html
+
+echo "Creating PDF $output_path..."
+
+weasyprint -s $css $html $output_path
+
+echo "Removing temp files..."
 
 rm $html
-rm _temp.xslt
+# rm _temp.xslt
 
 echo "Done."
