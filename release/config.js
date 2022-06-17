a6cc9eff478620a5521b09368d56e467711db6db
Sergei Averkiev <averoO@gmail.com>
2022-06-17 12:14:13 +0300

    Add release configs.

---
+++ release/config.js
@@ -0,0 +1,10 @@
+//Release frontend config
+
+export const API_URL = "/";
+export const DEFAULT_BATCHSIZE = 200;
+export const DEFAULT_VARIANTS_WINDOW_FROM = 20;
+export const DEFAULT_VARIANTS_WINDOW_TO = 20;
+
+export const TEST_LIMIT = 4 * DEFAULT_BATCHSIZE;
+
+export default API_URL;
